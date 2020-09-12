38c5698f9ff7323445038bffd5e8140a5a1fdfc8
Sean J. Bao <SeanB.9012@gmail.com>
2020-09-13 00:54:12 +0800

    Add local strategy

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import {
+  Controller,
+  Post, Request,
+  UseGuards,
+} from '@nestjs/common';
+import LocalAuthGuard from './local-auth.guard';
+
+@Controller('auth')
+export default class AuthController {
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import AuthService from './auth.service';
+import { PassportModule } from '@nestjs/passport';
 import UsersModule from '../users/users.module';
+import LocalStrategy from './local.strategy';
+import AuthController from './auth.controller';
+import AuthService from './auth.service';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy],
 })
 export default class AuthModule {}
