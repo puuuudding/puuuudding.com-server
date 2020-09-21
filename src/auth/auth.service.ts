af86eb508fce59fdb6f6a88ad9bc7d1b880b10f6
Sean J. Bao <SeanB.9012@gmail.com>
2020-09-21 14:23:05 +0800

    Reconstruct auth controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,5 @@
-import { Injectable, HttpStatus } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Response } from 'express';
 import { compare } from 'bcryptjs';
 import UsersService from 'users/users.service';
 import UserDto from 'users/dtos/user.dto';
@@ -11,7 +9,6 @@ export default class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
-    private configService: ConfigService,
   ) {}
 
   async validateUser(username: string, pwd: string): Promise<UserDto | null> {
@@ -23,20 +20,11 @@ export default class AuthService {
     return null;
   }
 
-  async login(user: UserDto, res: Response): Promise<void> {
+  async login(user: UserDto): Promise<string> {
     const payload = {
       sub: user.userId,
       username: user.username,
     };
-    const accessToken = this.jwtService.sign(payload);
-    res
-      .status(HttpStatus.OK)
-      .cookie('auth', accessToken, {
-        maxAge: 1e3 * 60 * 60 * 12,
-        httpOnly: true,
-        secure: this.configService.get('NODE_ENV') !== 'development',
-      })
-      .json(user)
-      .end();
+    return this.jwtService.sign(payload);
   }
 }
